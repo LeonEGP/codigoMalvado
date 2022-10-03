a9572ee1a58104a69ff9746033e758b259df2535
León G <A00573074@itesm.mx>
2022-10-03 22:50:26 +0000

    New

--- main.cpp
+++ main.cpp
@@ -5,7 +5,20 @@
 
 using namespace std;
 
+void espacio(){
+	cout << endl;
+}
+
 int main() {
 
+	cout << "INICIA PROGRAMA" << endl;
+	espacio();
+	cout << "Busqueda de codigo malicioso en transmisiones: " << endl;
+	espacio();
+	cout << "Posible codigo malicioso en cada transmision: " << endl;
+	espacio();
+	cout << "Datos comunes en transmisiones: " << endl;
+	espacio();
+	cout << "TERMINA PROGRAMA" << endl;
 	return 0;
 }
